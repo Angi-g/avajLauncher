f62cceecfdc2f662d1f35073bc2c1a53e6634c05
angi-g <dguyard@student.42.fr>
2020-10-31 23:05:46 +0100

    [WIP] change in updateConditions of the aircraft

--- fr/_42/avaj/simulator/vehicles/Helicopter.java
+++ fr/_42/avaj/simulator/vehicles/Helicopter.java
@@ -11,8 +11,36 @@ public class Helicopter extends Aircraft implements Flyable {
     }
 
     public void updateConditions() {
+        String weather = weatherTower.getWeather(this.coordinates);
 
+        if("SUN".equals(weather)) {
+            this.coordinates = new Coordinates(
+                    this.coordinates.getLongitude() + 10,
+                    this.coordinates.getLatitude(),
+                    this.coordinates.getHeight() + 2
+            );
+        } else if("RAIN".equals(weather)) {
+            this.coordinates = new Coordinates(
+                    this.coordinates.getLongitude() + 5,
+                    this.coordinates.getLatitude(),
+                    this.coordinates.getHeight()
+            );
+        } else if ("FOG".equals(weather)) {
+            this.coordinates = new Coordinates(
+                    this.coordinates.getLongitude() + 1,
+                    this.coordinates.getLatitude(),
+                    this.coordinates.getHeight()
+            );
+        } else if ("SNOW".equals(weather)) {
+            this.coordinates = new Coordinates(
+                    this.coordinates.getLongitude(),
+                    this.coordinates.getLatitude(),
+                    this.coordinates.getHeight() + 12
+            );
+        }
     }
+    //log
+    
 
     public void registerTower(WeatherTower weatherTower) {
 
