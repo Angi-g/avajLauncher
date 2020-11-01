6851eb72db028851e76af8c9b73eb6e5be091218
angi-g <dguyard@student.42.fr>
2020-11-01 19:57:35 +0100

    add exception

---
+++ fr/_42/avaj/simulator/InvalideArgumentException.java
@@ -0,0 +1,12 @@
+package fr._42.avaj.simulator;
+
+public class InvalideArgumentException extends Exception {
+
+    public InvalideArgumentException() {
+        super();
+    }
+
+    public InvalideArgumentException(String message) {
+        super(message);
+    }
+}
