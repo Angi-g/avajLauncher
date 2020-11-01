79178ed7ba014f1f87987c5b894751800a609e6c
angi-g <dguyard@student.42.fr>
2020-11-01 20:01:24 +0100

    complete Tower and logging

--- fr/_42/avaj/simulator/Tower.java
+++ fr/_42/avaj/simulator/Tower.java
@@ -7,17 +7,25 @@ import java.util.List;
 
 public class Tower {
 
-    private List<Flyable> observers = new ArrayList<Flyable>();
+    private List<Flyable> observers = new ArrayList<>();
 
     public void register(Flyable flyable) {
+        if (!observers.contains(flyable)) {
+            observers.add(flyable);
+        }
+        Logger.writeLine("Tower",flyable.whoAmI() + " registered to weather tower.");
 
     }
 
     public void unregister(Flyable flyable) {
+            observers.remove(flyable);
+        Logger.writeLine("Tower",flyable.whoAmI() + " unregistered from weather tower.");
 
     }
 
     protected void conditionChanged() {
-
+        for (Flyable observer : observers) {
+            observer.updateConditions();
+        }
     }
 }
